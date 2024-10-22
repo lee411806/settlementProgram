37618f23be5aad49481329211a9b551c4b45a866
Jdragon <lee411806@naver.com>
2024-10-22 14:09:39 +0900

    init commit

---
+++ src/test/java/com/sparta/settlementprogram/SettlementProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.settlementprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
